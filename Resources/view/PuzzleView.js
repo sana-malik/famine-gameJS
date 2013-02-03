e8246d32422eadff687092358c4adbee0e6cf25b
jetzm <jetzm81@gmail.com>
2013-02-03 11:40:10 -0500

    Added absolute time, removed redundant timer

--- Resources/view/PuzzleView.js
+++ Resources/view/PuzzleView.js
@@ -1,4 +1,6 @@
 function showPuzzleScreen(puzzle) {
+	var date = new Date();
+	
 	$("#main").empty();
 	$("#main").append(puzzle.getPuzzleHTML());
 
@@ -8,7 +10,8 @@ function showPuzzleScreen(puzzle) {
 	$.each(puzzle.hints, function(name, hint) {
 		$("#hints").append("<table border=\"0\"><tr><td><div>"+name+": </div></td><td><div id=\"" + nameToId(name) + "\" class=\"hint_counter\"></div></td></tr></table>");
 		if (!(hint.name in session.puzzleStats[puzzle.name].hintStats)) {
-			$(".hint_counter#"+nameToId(name)).countdown(hint.start_time*60-session.puzzleStats[puzzle.name]["sec_elapsed"], function() {alert("end!!!")});
+			var current_time = Math.round(date.getTime()/1000);
+			$(".hint_counter#"+nameToId(name)).countdown(hint.start_time*60-(current_time-session.puzzleStats[puzzle.name]["start_time"]), function() {alert("end!!!")});
 		}
 	});
 
