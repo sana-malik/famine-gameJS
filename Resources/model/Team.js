e0febb432f40e04da4e3106fdaf5bfacb343d2b8
Natalie <nnparisi@gmail.com>
2013-07-14 11:10:54 -0700

    Team popup formatting, a few other small style edits

--- Resources/model/Team.js
+++ Resources/model/Team.js
@@ -2,14 +2,14 @@ var Team = Backbone.Model.extend({
 
 	// the following two functions *should* be taken care of in TeamViews.js but too lazy right now...
 	getHTMLSummary : function() {
-		var output = this.getIconHTML() + "<span class=\"team_title\">" + this.get("name") + "</span>" + 
-		"<span class=\"team_bio\">" + this.get("bio") + "</span>";
+		var output = "<div class=\"team-popup-sidebar\">" + this.getIconHTML() + "Tributes: <ul><li>Glimmer</li><li>Glitter</li><li>Glamour</li></ul></div><div class=\"team-popup-content\"><h1 class=\"team_title\">" + this.get("name") + "</h1><h2 class=\"team-district\">District 1 - Luxury</h2><h2 class=\"team-status\">Status:  DEAD</h2>" + 
+		"<p class=\"team_bio\">" + this.get("bio") + "</p><p class=\"team-contact-info\">Contact: 123-456-7889</p></div>";
 		return output;
 	},
 
 	getIconHTML : function() {
 		var stat = "<div class=\"team_div\" id=\"" + this.get("id") +"\">" +
-		"<img src=\"images/team_icons/" + this.get("icon") + "\" class=\"team_img\">";
+		"<img src=\"images/team_icons/" + this.get("icon") + "\" class=\"team_img team-popup-img\">";
 		if (this.get("id") in session.get("teamStats") && session.get("teamStats")[this.get("id")]["status"] === teamStatus.DEAD) {
 			stat = stat + "<div class=\"status\">DEAD</div>";
 		}
