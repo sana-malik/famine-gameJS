8aa5a415fed7f77d78710c5d41ec3c3ae113aecf
Philip Dasler <philip.dasler@gmail.com>
2013-07-17 14:54:24 -0400

    Team pop-ups are dynamic now.

--- Resources/model/Team.js
+++ Resources/model/Team.js
@@ -2,8 +2,71 @@ var Team = Backbone.Model.extend({
 
 	// the following two functions *should* be taken care of in TeamViews.js but too lazy right now...
 	getHTMLSummary : function() {
-		var output = "<div class=\"team-popup-sidebar\">" + this.getIconHTML() + "Tributes: <ul><li>Glimmer</li><li>Glitter</li><li>Glamour</li></ul></div><div class=\"team-popup-content\"><h1 class=\"team_title\">" + this.get("name") + "</h1><h2 class=\"team-district\">District 1 - Luxury</h2><h2 class=\"team-status\">Status:  DEAD</h2>" + 
-		"<p class=\"team_bio\">" + this.get("bio") + "</p><p class=\"team-contact-info\">Contact: 123-456-7889</p></div>";
+		var district = String(this.get("id")).substring(0, this.get("id").length-1)
+		var dis_specialty = ""
+		switch(district){
+			case "1":
+				dis_specialty = "Luxury";
+				break;
+			case "2":
+				dis_specialty = "Masonry";
+				break;
+			case "3":
+				dis_specialty = "Technology";
+				break;
+			case "4":
+				dis_specialty = "Fishing";
+				break;
+			case "5":
+				dis_specialty = "Power";
+				break;
+			case "6":
+				dis_specialty = "Transportation";
+				break;
+			case "7":
+				dis_specialty = "Power";
+				break;
+			case "8":
+				dis_specialty = "Textiles";
+				break;
+			case "9":
+				dis_specialty = "Grain";
+				break;
+			case "10":
+				dis_specialty = "Livestock";
+				break;
+			case "11":
+				dis_specialty = "Agriculture";
+				break;
+			case "12":
+				dis_specialty = "Mining";
+				break;
+			default:
+				dis_specialty = "Unrecognized";
+		}
+
+		var output = "<div class=\"team-popup-sidebar\">" + 
+			this.getIconHTML() + 
+			"Tributes: <ul>";
+			
+			var members = this.get("members")
+			for( var index = 0; index < members.length; index++)
+				output = output + "<li>" + members[index] + "</li>" 
+
+			output = output + "</ul></div><div class=\"team-popup-content\"><h1 class=\"team_title\">" + 
+			this.get("name") + 
+			"</h1><h2 class=\"team-district\">District " + district + " - " + dis_specialty + "</h2>";
+
+			var status;
+			if($.extend({},session.get("teamStats"))[this.get("id")] == teamStatus.DEAD)
+				status = "Dead"
+			else
+				status = "Alive"
+
+			output = output + "<h2 class=\"team-status\">Status:  " + status + "</h2>" + 
+			"<p class=\"team_bio\">" + 
+			this.get("bio") + 
+			"</p><p class=\"team-contact-info\">Contact: 123-456-7889</p></div>";
 		return output;
 	},
 
