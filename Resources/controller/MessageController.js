cb8db02d8f96b604642a6acd9159f0016b7d716b
sana-malik <heyletsdance@gmail.com>
2013-09-22 00:33:35 -0400

    lengthened play time for notification sound

--- Resources/controller/MessageController.js
+++ Resources/controller/MessageController.js
@@ -33,7 +33,7 @@ var MessageController = {
 		});
 
 		// todo(sana): play a sound
-		playSound("pling.wav", 500);
+		playSound("pling.wav", 1000);
 
 		// increment activity tab counter
 		var unreadCount = session.get("unreadCount")+1;
